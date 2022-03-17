91524e27d0ad04039c77af64bcf827f5a8250068
luchoxx87 <albertotumadre2@gmail.com>
2022-03-17 15:44:13 -0300

    clases

---
+++ Cuenta/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cuenta
+{
+    public class Class1
+    {
+        int CBU {get;set;}
+        double Saldo {get;set;}
+
+
+    public Cuenta(int cbu, double saldo)
+    {
+        CBU = cbu;
+        Saldo = saldo;
+    }
+
+    public void Debitar(double monto)
+    {
+        Saldo -= monto;
+    }
+    public void Acreditar(double monto)
+    {
+        Saldo += monto;
+    }
+    }
+}
---
+++ EnteConSaldo.Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EnteConSaldo.Core
+{
+    public class Class1
+    {
+        
+    }
+}
